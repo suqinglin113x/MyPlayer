6fae21656255bf672f84747a89435f208afd5af7
SU <su@sudemac-mini.local>
2016-08-23 09:59:11 +0800

    Initial Commit

---
+++ MyPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyPlayer
+//
+//  Created by SU on 16/8/23.
+//  Copyright © 2016年 SU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyPlayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyPlayer
+//
+//  Created by SU on 16/8/23.
+//  Copyright © 2016年 SU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
